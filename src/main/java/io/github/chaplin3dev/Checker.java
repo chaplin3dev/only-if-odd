a31416386eec1aee7d8b2d986aa680178ece1ac5
chaplin3 <chaplin3@develomail.com>
2023-12-18 11:56:43 +0000

    Initial commit

---
+++ src/main/java/io/github/chaplin3dev/Checker.java
@@ -0,0 +1,17 @@
+package io.github.chaplin3dev;
+
+/**
+ * The class containing the isOdd method
+ */
+public class Checker 
+{
+    /**
+     * Checks whether a number is odd
+     * @param x The number to check
+     * @return Whether the number is odd
+     */
+    public static boolean isOdd( int x )
+    {
+        return x % 2 == 1;
+    }
+}
